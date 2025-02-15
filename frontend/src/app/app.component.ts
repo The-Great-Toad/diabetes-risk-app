0f8265c09a7aa11e6b8b6f496f42314fdddeb9f8
The-Great-Toad <bearcode26@gmail.com>
2025-02-15 22:18:50 +0100

    create components and routes

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
-import { MatInputModule } from '@angular/material/input';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatInputModule],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'frontend';
-}
+export class AppComponent {}
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { PatientListComponent } from './components/patient/patient-list/patient-list.component';
+import { PatientDetailComponent } from './components/patient/patient-detail/patient-detail.component';
+import { PatientFormComponent } from './components/patient/patient-form/patient-form.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  //   { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '/', component: AppComponent },
+  { path: 'patient-list', component: PatientListComponent },
+  { path: 'patient-detail', component: PatientDetailComponent },
+  { path: 'patient-form', component: PatientFormComponent },
+  { path: '**', component: PageNotFoundComponent },
+];
