f137ff6ebf736db0f08bc0db85d1e789e6323bc8
The-Great-Toad <bearcode26@gmail.com>
2025-04-04 17:51:02 +0200

    Microservice risk-assessment-service project set up

---
+++ risk-assesment-service/src/main/java/com/diabetesrisk/risk_assesment_service/RiskAssesmentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.diabetesrisk.risk_assesment_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiskAssesmentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiskAssesmentServiceApplication.class, args);
+	}
+
+}
---
+++ risk-assesment-service/src/test/java/com/diabetesrisk/risk_assesment_service/RiskAssesmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diabetesrisk.risk_assesment_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiskAssesmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
